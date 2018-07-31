4e113c600eda0a796c138faa9958c4da763a47ff
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-07-31 23:52:42 +0200

    Added Users & Finish styling

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
-  has_many :ingredients, inverse_of: :recipe
-  has_many :directions, inverse_of: :recipe
+  belongs_to :user
+
+  has_many :ingredients, dependent: :destroy
+  has_many :directions, dependent: :destroy
 
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
